b405d3ef8af9f646d13abb55373e870b8097a388
alexios <ale.ripreso@gmail.com>
2025-09-17 21:09:49 +0200

    aaa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"251f4a8d5b9f2577212b94379c1711dbdf4b7a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3772942263"
+    serviceWorkerVersion: "3221967417"
   }
 });
