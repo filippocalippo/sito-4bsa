b854e4a86c7bcc58767c1d2ef6c906efd3b0d9fd
alexios <ale.ripreso@gmail.com>
2025-09-19 18:53:24 +0200

    style fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"251f4a8d5b9f2577212b94379c1711dbdf4b7a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3221967417"
+    serviceWorkerVersion: "2039522173"
   }
 });
